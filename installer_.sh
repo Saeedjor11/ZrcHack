c1eb004eada589ab850b2c99e6adad1966b2017a
xscrprog <42752384+xscrprog@users.noreply.github.com>
2019-12-30 12:39:54 +0700

    Fix bugs

--- installer_.sh
+++ installer_.sh
@@ -1,13 +1,11 @@
 #!/data/data/com.termux/files/usr/bin/bash
-pkg up -y && pkg upgrade -y
+apt-get update
+apt-get  --assume-yes upgrade 
 
 su -c 'setenforce 0'
 
-time apt install tput rsync aapt neofetch toilet ncurses-utils tsu openssl-tool ruby wget lolcat bc -y
-
+apt-get  --assume-yes install tput rsync aapt neofetch toilet ncurses-utils tsu openssl-tool ruby wget lolcat bc
 tsu -c 'find . -iname '*scrz*' -exec rm -rf {} \;'
-
-
 
 tsu -c 'wget https://raw.githubusercontent.com/xscrprog/ZrcHack/master/allarchi/scrz_cli -O ./scrz_cli'
 tsu -c 'chmod a+x scrz_cli'
